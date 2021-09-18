bc96882edd92a0f983815f2ac42a1e6bbc3fcabc
puyuelin <1427977859@qq.com>
2021-09-18 11:02:44 +0800

    修改

--- src/components/Item/index.jsx
+++ src/components/Item/index.jsx
@@ -2,15 +2,37 @@ import React, { Component } from 'react';
 import './index.css'
 
 class Item extends Component {
+  state = { mouse: false }
+
+  handleMouse = (flag) => {
+    return () => {
+      this.setState({ mouse: flag })
+    }
+  }
+
+  handleCheck = (id) => {
+    return (event) => {
+      this.props.updateTodo(id, event.target.checked);
+    }
+  }
+
+  handleDelete = (id) => {
+    if (window.confirm("确定删除吗？")) {
+      this.props.deleteTodo(id);
+    }
+  }
+
   render() {
+    const { name, done, id } = this.props;
+    const { mouse } = this.state;
     return (
       <div>
-        <li>
+        <li style={{ backgroundColor: mouse ? "#ddd" : 'white' }} onMouseEnter={this.handleMouse(true)} onMouseLeave={this.handleMouse(false)}>
           <label>
-            <input type="checkbox" />
-            <span>xxxxx</span>
+            <input type="checkbox" checked={done} onChange={this.handleCheck(id)} />
+            <span>{name}</span>
           </label>
-          <button className="btn btn-danger" style={{ display: 'none' }}>删除</button>
+          <button onClick={() => this.handleDelete(id)} className="btn btn-danger" style={{ display: mouse ? "block" : "none" }}>删除</button>
         </li>
       </div>
     );
--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types'
+
 import Item from '../Item';
 import './index.css'
 
 class List extends Component {
+  static propTypes = {
+    todos: PropTypes.array.isRequired,
+    updateTodo: PropTypes.func.isRequired,
+    deleteTodo: PropTypes.func.isRequired
+  }
+
   render() {
+    const { todos, updateTodo, deleteTodo } = this.props;
     return (
       <div>
         <ul className="todo-main">
-          <Item />
+          {
+            todos.map(todo => {
+              return <Item key={todo.id} {...todo} updateTodo={updateTodo} deleteTodo={deleteTodo} />
+            })
+          }
         </ul>
 
       </div>
